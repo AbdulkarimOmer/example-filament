3da794fbd3dc9c2275231b7ab3431cfc2c56f76e
Abdulkarim Omer <ab.muhaureq@gmail.com>
2023-05-31 18:17:10 +0300

    remove lang/vendor dir

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -3,21 +3,19 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\PostResource\Pages;
-use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Post;
-use Filament\Forms;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
 use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class PostResource extends Resource
 {
     protected static ?string $model = Post::class;
+
+    // protected static ?string $modelLabel = __('filament/ar/pages/dashboard.1');
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
 
