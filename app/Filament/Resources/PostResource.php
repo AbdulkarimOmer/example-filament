d6686b93588588bb99405998b4c94ca88c665f86
Abdulkarim Omer <a.o.almuhaureq@bootfi.com>
2023-08-01 20:54:09 +0300

    Wip

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -50,12 +50,6 @@ class PostResource extends Resource
             ]);
     }
 
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
 
     public static function getPages(): array
     {
